eeafc950e0b4530a5f2a4d14b3f79b4a9e148e0b
Natthawut Jaimud <kmhahod@gmail.comgit config --global user.email kmhahod@gmail.com>
2026-01-06 15:13:09 +0700

    เพิ่ม public

--- ธนคาร.py
+++ ธนคาร.py
@@ -1,3 +1,21 @@
 class BankAcconut():
     def __init__(self):
         self.__balance = 0
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            print(f"ฝากสำเร็จ {amount} บาท ")
+        else :
+            print("ยอดเงินฝากต้องมากกว่า 0")
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__balance:
+            self.__balance -= amount
+            print(f'ถอนเงินสำเร็จ :  {amount} บาท')
+        else:
+            print(f'ยอดเงินไม่เพียงพอต่อการถอน {amount}') 
+    def check_balance(self):
+        return self.__balance
+    
+
+bankAcconut
